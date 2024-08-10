398fbdba0c6e510c47bfe9ddf53594753e4dbc0e
ElStabilini <el.stabilini@gmail.com>
2024-08-10 12:17:05 +0200

    upgraded environment, DQNAgent to store environment info and changed training script to save environement info during training

--- TestDQN.py
+++ TestDQN.py
@@ -8,8 +8,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import random
 import os
-import timeit
 import datetime
+import pickle
+
 from pathlib import Path
 from tqdm import tqdm
 
@@ -22,34 +23,43 @@ TrainedDQLearning_data = Path('/home/elisa/Desktop/Uni/SecondY/RL/SecondPart/Sum
 TrainedDQLearning_plots = Path('/home/elisa/Desktop/Uni/SecondY/RL/SecondPart/SummerClaude/TrainedDQLearning/Plots') 
 
 #initialize agent and environment
-env = gym.make('VFAFarm-v0.1')
+env = gym.make('VFAFarm-v0.0.1')
 agent = DQNAgent(env)
 
 agent = DQNAgent(env)
-num_episodes = 1000
+num_episodes = 100
 batch_size = 32
 
 # Lists to store rewards and episode lengths
 rewards = []
 episode_lengths = []
+episode_info = []
 
 for episode in range(num_episodes):
     state = env.reset()
     done = False
     total_reward = 0
     steps = 0
+    episode_additional_info = {}
 
     while not done:
         action = agent.act(state)
-        next_state, reward, done, _ = env.step(action)
+        next_state, reward, done, info = env.step(action)
         agent.train(state, action, reward, next_state, done)
         state = next_state
         total_reward += reward
         steps += 1
 
+        # Accumulate info for the episode
+        for key, value in info.items():
+            if key not in episode_additional_info:
+                episode_additional_info[key] = []
+            episode_additional_info[key].append(value)
+
     # Store the total reward and episode length
     rewards.append(total_reward)
     episode_lengths.append(steps)
+    episode_info.append(episode_additional_info)
 
     if episode % 10 == 0:
         agent.update_target_model()
@@ -78,3 +88,6 @@ filename = f"episode_length_{formatted_time}.npy"
 if not os.path.exists(TrainedDQLearning_data):
     os.makedirs(TrainedDQLearning_data)
 np.save(os.path.join(TrainedDQLearning_data,filename), episode_lengths)
+
+with open(os.path.join(TrainedDQLearning_data, f"episode_info_{formatted_time}.pkl"), 'wb') as f:
+    pickle.dump(episode_info, f)
