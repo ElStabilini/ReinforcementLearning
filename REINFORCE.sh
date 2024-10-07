2914f58be165c4b6d89e4094eee7bced001878f3
ElStabilini <el.stabilini@gmail.com>
2024-10-07 09:39:45 +0200

    corrected SBATCH syntax in sh file

--- REINFORCE.sh
+++ REINFORCE.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name = REINFORCE  # Job name
-#SBATCH --nodelist = meitner, lorentz, levicivita, dirac, poincare, turing, penrose, weyl, heaviside            # Node to use
+#SBATCH --job-name=REINFORCE  # Job name
+#SBATCH --nodelist=meitner,lorentz,levicivita,dirac,poincare,turing,penrose,weyl,heaviside  # Nodes to use
 
 python3 training_REINFORCE.py
